04982ad584b885f25cdacd04c2fd154d62cb7d87
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-01-29 15:29:27 -0500

    complete lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  //Set the useState to All bc that is what will show all categories without filtering
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  //Filter the items to only display the items if their category matches the selected Category
+  const filteredItems = items.filter(item => {
+    if(selectedCategory === "All"){
+      return true
+    }
+    return item.category === selectedCategory
+  })
+  console.log(filteredItems)
+
   return (
-    <div className="ShoppingList">
+    <div className="ShoppingList" items={filteredItems}>
       <div className="Filter">
-        <select name="filter">
+        <select onChange={(event) => setSelectedCategory(event.target.value)} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,8 +26,8 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+        {filteredItems.map((item) => (
+            <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
     </div>
@@ -22,3 +35,32 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
+
+
+
+
+// function PetPage(){
+
+//   const [searchText, setSearchText] = useState("")
+
+//   const filteredPets = pets.filter(pet => {
+//       return pet.name.toUpperCase().includes(searchText.toUpperCase())
+//   })
+//   console.log(filteredPets)
+
+//   return (
+//       <main>
+//           <div className="searchbar">
+//               <label htmlFor="search">Search Pets:</label>
+//               <input
+//                   type="text"
+//                   id="search"
+//                   placeholder="Type a name to search..."
+//                   onChange={(event) => setSearchText(event.target.value)}
+//               />
+//           </div>
+//           <PetList pets={filteredPets}/>
+//       </main>
+//   );
+// }
